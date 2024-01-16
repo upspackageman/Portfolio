a64896d9a4c630412bcfd73969adcfdd3e9bddb0
William Ward <wward246@gmail.com>
2024-01-15 18:09:19 -0800

    ddd

--- API/venv/message.py
+++ API/venv/message.py
@@ -16,7 +16,7 @@ class EmailPayload(BaseModel):
 # Add CORS middleware
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],  # You may want to restrict this to specific origins in production
+    allow_origins=["http://207.246.108.92","http://207.246.108.92","https://207.246.108.92"],  # You may want to restrict this to specific origins in production
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
