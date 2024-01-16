a3a85caa314a266f765aba36e0eefd1a2a2848c5
William Ward <wward246@gmail.com>
2024-01-15 22:08:00 -0800

    duh

--- portfolio/src/environments/environments.production.ts
+++ portfolio/src/environments/environments.production.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
     production: false,
-    apiUrl:'http://207.246.108.92/:8000/', //http://192.168.0.28:5001/api/
+    apiUrl:'http://207.246.108.92:8000/', //http://192.168.0.28:5001/api/
   };
   
   /*
